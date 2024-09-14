3b6c8d161922c96d8b357902940dacdc9c47bfbf
Bele Archis Anant <belearchis.04@gmail.com>
2024-09-14 23:16:54 +0530

    DSA

---
+++ src/Poweroffour.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Poweroffour {
+    public static void main(String[] args) {
+        try(Scanner sc=new Scanner(System.in)){
+            System.out.println("Enter the number : ");
+            int n=sc.nextInt();
+            System.out.println(isPowerOfFour(n));
+        }
+    }
+   
+    static public boolean isPowerOfFour(int n) {
+        if(n==1){
+            return true;
+        }
+        int x=0;
+        double res=0;
+        if(n>0){
+            for(double i=1;i<n;i++){
+                res=Math.pow(4,i);
+                if(res>n){
+                    return false;
+                }
+                if(n==res){
+                    return true;
+                }
+            }
+        }
+        return false;
+    
+}
+}
