a41bf61d1361d623999ed86e9d26eba29bcf716a
Bele Archis Anant <belearchis.04@gmail.com>
2024-09-21 23:27:04 +0530

    DSA

---
+++ src/Lexicographicnumbers.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+public  class Lexicographicnumbers {
+    public static void main(String[] args) {
+        try(Scanner sc=new Scanner(System.in)){
+            int n=sc.nextInt();
+            System.out.println(lexicalOrder(n));
+        }
+    }
+
+    
+    public static List<Integer> lexicalOrder(int n) {
+    List<Integer> al = new  ArrayList<>();
+    int curr = 1; 
+    for(int i=1; i<=n; i++)
+    {
+      al.add(curr);
+      if(curr*10<=n)
+      curr = curr*10;
+      else
+      {
+        while(curr%10==9 || curr>=n)
+        {
+          curr = curr/10;   
+        } 
+        curr += 1;
+      }
+    } 
+    return al;
+
+
+}
+}
